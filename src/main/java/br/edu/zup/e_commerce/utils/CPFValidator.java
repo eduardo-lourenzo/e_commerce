445c31b696c063ad4fa5ccf69f296b7b91b89036
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-01-30 11:56:27 -0300

    feat(utils): create CPFValidator class

---
+++ src/main/java/br/edu/zup/e_commerce/utils/CPFValidator.java
@@ -0,0 +1,48 @@
+package br.edu.zup.e_commerce.utils;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public class CPFValidator {
+    private CPFValidator() {
+    }
+
+    public static boolean isValidCPF(
+            @NotBlank(message = "O CPF é obrigatório.")
+            @Pattern(regexp = "\\d{11}", message = "O CPF deve conter exatamente 11 dígitos.")
+            String cpf
+    ) {
+        int firstVerifierDigit = getDigitAt(cpf, 9);
+        int secondVerifierDigit = getDigitAt(cpf, 10);
+
+        // && Operador AND com curto-circuito (não testa a segunda parte, se a primeira for falsa).
+        return isValidVerifierDigit(cpf, 9, firstVerifierDigit)
+                && isValidVerifierDigit(cpf, 10, secondVerifierDigit);
+    }
+
+    private static int getDigitAt(String cpf, int position) {
+        return Character.getNumericValue(cpf.charAt(position));
+    }
+
+    private static int getSum(String partialCPF) {
+        int sum = 0;
+        int factor = partialCPF.length() + 1;
+
+        for (int index = 0; index < partialCPF.length(); index++) {
+            sum += factor * getDigitAt(partialCPF, index);
+            factor--;
+        }
+
+        return sum;
+    }
+
+    private static int modulusEleven(int sum) {
+        int remainder = sum % 11;
+        return (remainder < 2) ? 0 : 11 - remainder;
+    }
+
+    private static boolean isValidVerifierDigit(String cpf, int length, int verifierDigit) {
+        int calculatedDigit = modulusEleven(getSum(cpf.substring(0, length)));
+        return calculatedDigit == verifierDigit;
+    }
+}
