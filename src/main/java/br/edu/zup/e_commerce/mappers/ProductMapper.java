f0e0611ff86302ec6e579a6a94ad9c06eed8f64c
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-02-01 20:46:58 -0300

    feat(mappers): create ProductMapper class

---
+++ src/main/java/br/edu/zup/e_commerce/mappers/ProductMapper.java
@@ -0,0 +1,27 @@
+package br.edu.zup.e_commerce.mappers;
+
+import br.edu.zup.e_commerce.dtos.ProductRequestDTO;
+import br.edu.zup.e_commerce.dtos.ProductResponseDTO;
+import br.edu.zup.e_commerce.models.Product;
+
+public class ProductMapper {
+    private ProductMapper() {
+        throw new UnsupportedOperationException("Esta é uma classe mapeadora e não pode ser instanciada");
+    }
+
+    public static Product fromDTOToEntity(ProductRequestDTO requestDTO){
+        return new Product(
+                requestDTO.getName(),
+                requestDTO.getPrice(),
+                requestDTO.getQuantity()
+        );
+    }
+
+    public static ProductResponseDTO fromEntityToDTO(Product entity){
+        return new ProductResponseDTO(
+                entity.getName(),
+                entity.getPrice(),
+                entity.getQuantity()
+        );
+    }
+}
